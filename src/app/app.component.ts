9a7d0116933721313d60c1f56e26731b7e59396f
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 22:52:36 +0100

    Interpolación de Objetos. Ver como nos pre detecta si una variable no existe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'curso-angular';
+  persona = {
+    nombre: 'Pedro',
+    apellido:'Nieto',
+    hijos: undefined
+  }
 }
