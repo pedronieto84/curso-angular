96c5563cb3aa64107ef2374be9c3b9450af7dcca
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-16 23:06:01 +0100

    Interpolation: Valid Javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Persona {
+  nombre: string
+  apellido: string
+  hijos: undefined| Persona
+  edad: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  persona = {
+
+  
+  persona: Persona = {
     nombre: 'Pedro',
     apellido:'Nieto',
-    hijos: undefined
+    hijos: undefined,
+    edad: 39
+  }
+
+  restarleEdad(persona: Persona, edad: number){
+    return persona.edad - edad
   }
 }
