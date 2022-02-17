cb867c198adb9768d1ace5f56252bf4a8e32d177
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-17 19:53:11 +0100

    HTTP Interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { InterceptorsService } from './services/interceptors/interceptors.service';
 @NgModule({
   declarations: [
     AppComponent
@@ -15,7 +16,9 @@ import { HttpClientModule } from '@angular/common/http'
     HttpClientModule // Este es el módulo que me da acceso al HttpClient Service
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: InterceptorsService, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
