f898ef12b9408c24328bdaecef556dc8f8cb084e
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-17 23:51:42 +0100

    Routing Get Full Url. Comprobar que el componente no se destruye.

--- src/app/components/detalle-animal/detalle-animal.component.ts
+++ src/app/components/detalle-animal/detalle-animal.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-animal',
   templateUrl: './detalle-animal.component.html',
   styleUrls: ['./detalle-animal.component.scss'],
 })
-export class DetalleAnimalComponent implements OnInit {
+export class DetalleAnimalComponent implements OnInit, OnDestroy {
   animal!: string;
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private routeServ: Router) {}
 
   ngOnInit(): void {
-    this.route.queryParams
-    .subscribe((params) => {
+    console.log('ngOnInit');
+    this.route.url.subscribe((params) => {
       console.log('queryParams', params);
+      console.log('full url', this.routeServ.url);
     });
+
+    // Porfavor fíjate que el componente no se destruye ni se reconstruye. Lo único que cambia es la url. Eso nos obliga a programar de una forma concreta.
+
   }
+
+  ngOnDestroy(): void {
+    console.log('onDestroy');
+  }
+
+
 }
