64749a072a6d6f3f69b5944282a1c734c936fa77
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-17 20:39:53 +0100

    Navegación Programática

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './services/data/data.service'
-
+import { Router } from '@angular/router';
+import { DataService } from './services/data/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
+export class AppComponent {
+  animales = ['perro', 'gato', 'pajaro', 'leon'];
 
-  animales = [
-    'perro', 'gato', 'pajaro', 'leon'
-  ]
-  
-  constructor(){}
-
-
-
-  
+  constructor(private router: Router) {}
+ // Router es para navegar programáticamente
+ 
+  navegar(url: string, url2: string) {
+    const confirmar = prompt('Password?');
+    const password = 1234;
+    if (confirmar === password.toString()) {
+      this.router.navigate([url, url2]);
+    } else {
+      return;
+    }
+  }
 }
