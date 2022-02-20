997d0b1b4da0dd6f980d924189a45339683ef15d
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-20 01:00:19 +0100

    Creo todas las rutas e importo el Formularios Module a  app.module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArrayFormComponent } from './components/forms/array-form/array-form.component';
+import { BasicFormComponent } from './components/forms/basic-form/basic-form.component';
+import { NestedFormComponent } from './components/forms/nested-form/nested-form.component';
+import { SubmitFormComponent } from './components/forms/submit-form/submit-form.component';
+import { ValidFormComponent } from './components/forms/valid-form/valid-form.component';
 
 const routes: Routes = [
   {
-    
-  }
+    path: '',
+    component: BasicFormComponent,
+  },
+  {
+    path: 'array',
+    component: ArrayFormComponent,
+  },
+  {
+    path: 'nested',
+    component: NestedFormComponent,
+  },
+  {
+    path: 'submit',
+    component: SubmitFormComponent,
+  },
+  {
+    path: 'valid',
+    component: ValidFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
