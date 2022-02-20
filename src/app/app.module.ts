43f65603662e16cb45b4b45358a633a792bbff07
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-20 01:04:50 +0100

    Importo MatMenuModule pero me da error de missing module CDK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, MatMenuModule],
   providers: [],
   bootstrap: [AppComponent],
 })
