3b0bd79b68951b0d7c0bbee500bd531c8ff28d2a
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-20 01:35:15 +0100

    Instalo por Ng Add el @angular/material y dejo preparada la navegación Básica.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, MatMenuModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
