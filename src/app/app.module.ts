dc32df83d2ddc74da57861690e9abd6880ec6146
pedronieto84 <pedro.nieto.sanchez@gmail.com>
2022-02-23 11:46:37 +0100

    Importo la libreria en la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LoginIcaro2302Module } from 'projects/login-icaro2302/src/public-api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginIcaro2302Module
   ],
   providers: [],
   bootstrap: [AppComponent]
